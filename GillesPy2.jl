3112adc1a7881c41a71642a8522eb278550198b7
seanebum <smatthe2@unca.edu>
2020-07-27 12:46:00 -0400

    added constructors and example for events/triggers/assignments

--- GillesPy2.jl
+++ GillesPy2.jl
@@ -13,9 +13,13 @@ function __init__()
             self.timespan(numpy.linspace(0,100,101))  
     """
 end
+
 model() = py"Model"()
 parameter(name, expression) = py"gillespy2.Parameter"(name=name, expression=expression)
 species(name, initial_value) = py"gillespy2.Species"(name=name, initial_value=initial_value)
 reaction(name, reactants, products, rate) = py"gillespy2.Reaction"(name=name, reactants=reactants, products=products, rate=rate)
+event_trigger(expression) = py"gillespy2.EventTrigger"(expression=expression)
+event_assignment(variable, expression) = py"gillespy2.EventAssignment"(variable=variable, expression=expression)
+event(name, trigger, assignments) = py"gillespy2.Event"(name=name, trigger=trigger, assignments=assignments)
 
 end
